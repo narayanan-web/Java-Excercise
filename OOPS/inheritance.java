2bca9af236b6b3f591093fee672e9a529f16f54f
anarayanan345@gmail.com <anarayanan345@gmail.com>
2021-09-15 16:10:59 +0530

    create car-game

--- OOPS/inheritance.java
+++ OOPS/inheritance.java
@@ -1,5 +1,7 @@
 package OOPS;
 
+import java.util.Scanner;
+
 class car {
 	public int gear;
 	public int speed;
@@ -10,14 +12,13 @@ class car {
 		this.speed = speed;
 	}
 
-	public void brake(int decrement)
+	public void brake()
 	{
-		speed -= decrement;
+		speed -= 50;
 	}
-
-	public void accelarate(int increment)
+	public void accelarate()
 	{
-		speed += increment;
+		speed += 50;
 	}
 
 	public String toString()
@@ -35,22 +36,58 @@ class raceCar extends car {
 	public raceCar(int gear, int speed, int nitro)
 	{
 		super(gear, speed);
-		speed *= nitro; 
+		speed *= nitro;
+		this.speed = speed;
 	}
 
 	// overriding toString() method of car
 	@Override public String toString()
 	{
-		return (super.toString() + "\nspeed after nitro is " + super.speed);
+		return (super.toString());
 	}
 }
 
 // driver class
 public class inheritance {
+	
+	static Scanner nar = new Scanner(System.in);
+
+	static void control(car xCar)
+	{
+		while(true)
+		{
+			System.out.println("If you want to accelarate press 1 or Brake press 0, stop the drive press 9");
+			int option = nar.nextInt();
+			if(option==1)
+			{
+				xCar.accelarate();
+				System.out.println(xCar.toString());
+			}
+			else if(option==0)
+			{
+				xCar.brake();
+				System.out.println(xCar.toString());
+			}
+			else
+				break;
+		}
+	}
 	public static void main(String args[])
 	{
+		car c = new car(2, 50);
 		raceCar rc = new raceCar(3, 100, 2);
-		System.out.println(rc.toString());
+		System.out.println("If you want racecar or normal car");
+		char t = nar.next().charAt(0);
+		if(t=='r')
+		{
+			System.out.println(rc.toString());
+			control(rc);
+		}
+		else
+		{
+			System.out.println(c.toString());
+			control(c);
+		}
 	}
 }
 
